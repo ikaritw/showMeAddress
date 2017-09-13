a2785fd706e5f38c6c4f866321aa6dcdbbc8157d
pi@raspberry-zero-w <ikaritw@gmail.com>
2017-09-13 12:00:06 +0800

    加入更新date的方法，避免重開機後timesync無法使用造成時間錯誤，會讓某些https呼叫失敗

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
 /* jshint eqeqeq:false,eqnull:true,node:true */
 
+function syncTime(callback) {
+	callback = callback || function() {};
+	const request = require('request');
+	const moment = require('moment');
+	const {
+		exec
+	} = require('child_process');
+
+	/*
+	{
+		"status": "OK",
+		"message": "",
+		"zones": [{
+			"countryCode": "TW",
+			"countryName": "Taiwan",
+			"zoneName": "Asia/Taipei",
+			"gmtOffset": 28800,
+			"timestamp": 1501597479
+		}]
+	}
+	*/
+	var timezonedbURL = "https://api.timezonedb.com/v2/list-time-zone?key=Q7YRX0AV3030&format=json&country=TW";
+	request(timezonedbURL, function(error, response, body) {
+		if (error) {
+			console.error(error);
+			return;
+		}
+
+		console.log(body);
+		var info = JSON.parse(body);
+		var zone = info.zones[0];
+		var currentTime = new Date((zone.timestamp - zone.gmtOffset) * 1000);
+		var currentMoment = moment(currentTime);
+
+		var cmd = "date --set '" + currentMoment.format() + "'";
+		console.log(cmd);
+		exec(cmd, function(err, stdout, stderr) {
+			if (err) {
+				// node couldn't execute the command
+				console.error(err);
+				return;
+			}
+
+			// the *entire* stdout and stderr (buffered)
+			console.log('stdout:' + stdout);
+			console.log('stderr:' + stderr);
+			if (callback) {
+				callback(currentMoment);
+			}
+		});
+	});
+}
+
 /**
  * 取得系統ip address
  * @param  {[type]} os [description]
@@ -8,9 +61,9 @@
 function getip(os) {
 	var ifaces = os.networkInterfaces();
 	var ipAddress = [];
-	Object.keys(ifaces).forEach(function(ifname) {
+	Object.keys(ifaces).forEach(function (ifname) {
 		var alias = 0;
-		ifaces[ifname].forEach(function(iface) {
+		ifaces[ifname].forEach(function (iface) {
 			if ('IPv4' !== iface.family || iface.internal !== false) {
 				// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
 				return;
@@ -29,29 +82,39 @@ function getip(os) {
 	return ipAddress;
 }
 
-//取得webhookurl資訊
-var webhookKey;
-if (process.argv.length > 2) {
-	webhookKey = process.argv[2]; // XXX/YYY/ZZZ
-} else {
-	var config = require('./config.json');
-	webhookKey = config.webhookKey;
+function callSlack() {
+	//取得webhookurl資訊
+	var webhookKey;
+	if (process.argv.length > 2) {
+		webhookKey = process.argv[2]; // XXX/YYY/ZZZ
+	} else {
+		var config = require('./config.json');
+		webhookKey = config.webhookKey;
+	}
+	var webhookUri = "https://hooks.slack.com/services/" + webhookKey;
+
+	//取得系統資訊
+	var os = require('os');
+	var username = os.hostname();
+	var text = getip(os).join(";");
+
+	//發送slack
+	var Slack = require('slack-node');
+	var slack = new Slack();
+	slack.setWebhook(webhookUri);
+	slack.webhook({
+		channel: "@jazz",
+		username: username,
+		text: text
+	}, function (err, response) {
+		console.log(response);
+	});
 }
-var webhookUri = "https://hooks.slack.com/services/" + webhookKey;
 
-//取得系統資訊
-var os = require('os');
-var username = os.hostname();
-var text = getip(os).join(";");
+function main() {
+	syncTime(function () {
+		callSlack();
+	});
+}
 
-//發送slack
-var Slack = require('slack-node');
-var slack = new Slack();
-slack.setWebhook(webhookUri);
-slack.webhook({
-	channel: "@jazz",
-	username: username,
-	text: text
-}, function(err, response) {
-	console.log(response);
-});
+main();
---
+++ readme.md
@@ -0,0 +1,26 @@
+## showMeAddress
+
+> nano /etc/rc.local
+
+	#!/bin/sh -e
+	#
+	# rc.local
+	#
+	# This script is executed at the end of each multiuser runlevel.
+	# Make sure that the script will "exit 0" on success or any other
+	# value on error.
+	#
+	# In order to enable or disable this script just change the execution
+	# bits.
+	#
+	# By default this script does nothing.
+	
+	# Print the IP address
+	_IP=$(hostname -I) || true
+	if [ "$_IP" ]; then
+	  printf "My IP address is %s\n" "$_IP"
+	  ## send ip to slack
+	  /usr/bin/node /home/pi/showMeAddress/app.js
+	fi
+	
+	exit 0
